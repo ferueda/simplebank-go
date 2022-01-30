3a1e209f24ab48a8794cc9029dab3bc64d002c8b
ferueda <fe.rueda@outlook.com>
2022-01-29 19:28:06 -0800

    Create JWT tokens logic and test suite

---
+++ token/main_test.go
@@ -0,0 +1,27 @@
+package token
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const (
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
